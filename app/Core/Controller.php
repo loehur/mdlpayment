dedbd2a0b92612986efda9f2356bd3cf6cc75e97
loehur <47594852+loehur@users.noreply.github.com>
2022-10-23 14:40:21 +0700

    list harga

--- app/Core/Controller.php
+++ app/Core/Controller.php
@@ -111,7 +111,7 @@ class Controller extends Public_Variables
         $return['data_pre'] = $data['prepaid'];
         $return['total_pre'] = $total_pre_success;
 
-        if (($saldo + 5000) > $this->setting['min_saldo']) {
+        if ($saldo >= $this->setting['min_saldo']) {
             $this->margin_prepaid = $this->margin_prepaid1;
             $this->admin_postpaid = $this->admin_postpaid1;
         } else {
