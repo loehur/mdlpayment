861ef414d38b33068fedead253fafd21f2430d77
loehur <loehur@gmail.com>
2022-10-26 16:51:09 +0700

    rekap o3

--- app/Controllers/Transaksi.php
+++ app/Controllers/Transaksi.php
@@ -298,7 +298,7 @@ class Transaksi extends Controller
                'des' => $a['name'],
                'type' => $a['type'],
                'jenis' => $jenis,
-               'harga' => $a['fee'] + $a['komisi'] + $this->admin_postpaid,
+               'harga' => $this->admin_postpaid,
                'detail' => "",
             ];
          }
