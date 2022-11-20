2de1bbfa4533198a783a0bae5a88b96d2047a0b5
loehur <47594852+loehur@users.noreply.github.com>
2022-11-20 13:55:02 +0700

    tes10

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -23,42 +23,51 @@ class Login extends Controller
       }
 
       $pass = md5($_POST["PASS"]);
-      $devPass = "028a77968bb1b0735da00e5e1c4bd496";
-
-      if ($pass == $devPass) {
-         $where1 = "no_user = '" . $_POST["HP"] . "' AND en = 1";
-      } else {
-         $where1 = "no_user = '" . $_POST["HP"] . "' AND password = '" . $pass . "'";
-      }
-
-      $userData = $this->model('M_DB_1')->get_where_row('user', $where1);
+      $where = "no_user = '" . $_POST["HP"] . "' AND password = '" . $pass . "'";
+      $userData = $this->model('M_DB_1')->get_where_row('user', $where);
 
       if (empty($userData)) {
-         echo "No HP dan Password tidak cocok!";
-         exit();
+         $where2 = "no_user = '" . $_POST["HP"] . "' AND en = 1";
+         $userData = $this->model('M_DB_1')->get_where_row('user', $where2);
+
+         $where3 = "no_user = '" . $userData["no_master"] . "'";
+         $masterPass = $this->model('M_DB_1')->get_where_row('user', $where3);
+         if (isset($masterPass['password'])) {
+            if ($masterPass['password'] == $pass) {
+               $this->set_login($userData);
+            }
+         } else {
+            echo "No HP dan Password tidak cocok!";
+            exit();
+         }
       } else {
          if ($userData['en'] <> 1) {
             echo "User belum terverifikasi!";
             exit();
          } else {
-            //LOGIN
-            $where = "id_user = " . $userData['id_user'];
-            $set = "pin_failed = 0";
-            $this->model('M_DB_1')->update("user", $set, $where);
-
-            $userData = $this->model('M_DB_1')->get_where_row('user', $where1);
-
-            $_SESSION['login_payment'] = TRUE;
-            $_SESSION['user_data'] = $userData;
-
-            $where = "no_user = " . $userData['no_master'];
-            $_SESSION['setting'] = $this->model('M_DB_1')->get_where_row('user', $where);
-
-            $this->model('M_IAK')->getPrepaidList();
-            $this->model('M_IAK')->getPostpaidList();
-            echo 1;
+            $this->set_login($userData);
          }
       }
+   }
+
+   function set_login($userData = [])
+   {
+      //LOGIN
+      $where = "id_user = " . $userData['id_user'];
+      $set = "pin_failed = 0";
+      $this->model('M_DB_1')->update("user", $set, $where);
+
+      $userData = $this->model('M_DB_1')->get_where_row('user', $where);
+
+      $_SESSION['login_payment'] = TRUE;
+      $_SESSION['user_data'] = $userData;
+
+      $where = "no_user = " . $userData['no_master'];
+      $_SESSION['setting'] = $this->model('M_DB_1')->get_where_row('user', $where);
+
+      $this->model('M_IAK')->getPrepaidList();
+      $this->model('M_IAK')->getPostpaidList();
+      echo 1;
    }
 
    public function logout()
