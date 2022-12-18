dff983b5071f83e7b6c814a63ac42d54dce41514
loehur <47594852+loehur@users.noreply.github.com>
2022-12-18 08:13:15 +0700

    REKAP

--- app/Views/Rekap/data.php
+++ app/Views/Rekap/data.php
@@ -1,4 +1,4 @@
-<div class="content">
+<div class="content" style="padding-bottom: 70px;">
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-6 border pb-1">
