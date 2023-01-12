1f851c65c3aea85ae99f98e4063b541ee0bd12e6
loehur <47594852+loehur@users.noreply.github.com>
2023-01-12 14:17:57 +0700

    dfa

--- app/Controllers/Set.php
+++ app/Controllers/Set.php
@@ -1,8 +1,22 @@
 <?php
 class Set extends Controller
 {
+   public function __construct()
+   {
+      $this->data();
+      $this->session_cek();
+   }
+
    public function index()
    {
+      if (isset($_SESSION['login_payment'])) {
+         if ($_SESSION['login_payment'] == false || $_SESSION['user_data']['no_user'] <> '081268098300') {
+            exit();
+         }
+      } else {
+         exit();
+      }
+
       $this->view(__CLASS__ . '/main');
    }
 }
