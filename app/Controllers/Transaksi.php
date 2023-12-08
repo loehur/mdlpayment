50a2a61ef2d60377816bc000daedb606b6ced1f7
loehur <loehur@gmail.com>
2023-12-08 10:49:17 +0700

    mantap ini udah pakai waRp

--- app/Controllers/Transaksi.php
+++ app/Controllers/Transaksi.php
@@ -405,7 +405,7 @@ class Transaksi extends Controller
          if (strlen($note) > 0) {
             $note = "_" . $note . "_\n";
          }
-         $text = $target . "\n" . strtoupper($target_name) . "\n" . number_format($jumlah) . " Adm. " . number_format($biaya) . "\n" . $note . "payment.mdl.my.id/O/m/" . $id_manual;
+         $text = $target . "\n" . strtoupper($target_name) . "\nRp" . number_format($jumlah) . " Adm. Rp" . number_format($biaya) . "\n" . $note . "payment.mdl.my.id/O/m/" . $id_manual;
          $text_final = $transaksi . $text;
 
          //SEND WA
